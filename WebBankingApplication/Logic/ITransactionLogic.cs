5bb24d35df8d0ae5f452c2659c061c6347f29737
draven-mcconathy <draveniii@gmail.com>
2024-11-12 21:44:23 -0500

    Added share level transaction logic for internal transfers.

---
+++ WebBankingApplication/Logic/ITransactionLogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebBankingApplication.Logic
+{
+    public interface ITransactionLogic
+    {
+        Boolean WithdrawFunds(Int32 AmountToWithdraw);
+        Boolean DepositFunds(Int32 AmountToDeposit);
+        Boolean InternalTransfer(Int32 TransferAmount, String AccountToTransferFrom, String AccountToTransferTo);
+
+
+    }
+}
