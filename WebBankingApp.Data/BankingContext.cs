2f39c782cf5c9dd46794655fd41fe5c38288e251
draven-mcconathy <draveniii@gmail.com>
2024-11-17 14:05:24 -0500

    Added BankingContext (dbContext).

---
+++ WebBankingApp.Data/BankingContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Transactions;
+using WebBankingApp.Models;
+
+namespace WebBankingApp.Data
+{
+    public class AccountContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Share> Shares { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Models.Transaction> Transactions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite();
+        }
+    }
+}
