9d2db1bb4f3149d9088b2acfdf6cdeefea99b241
draven-mcconathy <draveniii@gmail.com>
2024-11-21 19:07:36 -0500

    Added MemberRepository and Interface.

---
+++ WebBankingApp.Data/MemberRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebBankingApp.Models;
+
+namespace WebBankingApp.Data
+{
+    public class MemberRepository
+    {
+        private readonly BankingContext _dbContext;
+
+        public MemberRepository()
+        {
+            _dbContext = new BankingContext();
+        }
+
+        public void AddMember(Member newMember)
+        {
+            _dbContext.Members.Add(newMember);
+            _dbContext.SaveChanges();
+        }
+
+        public GetAccounts(Member member)
+        {
+        }
+
+    }
+}
