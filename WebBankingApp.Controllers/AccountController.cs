30969de40856fa8aa839a4ab6cebe7d680daafd3
draven-mcconathy <draveniii@gmail.com>
2024-11-21 19:06:29 -0500

    Added Controllers Project.

---
+++ WebBankingApp.Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using WebBankingApp.Data;
+using WebBankingApp.Models;
+using System.Text.Json;
+
+namespace WebBankingApp.Controllers
+{
+    public class AccountController
+    {
+        private readonly IAccountRepository _accountRepo;
+
+        public AccountController(IAccountRepository accountRepo)
+        {
+            _accountRepo = accountRepo;
+        }
+
+        public Boolean CreateNewAccount(String jsonInput)
+        {
+            Account newAccount = JsonSerializer.Deserialize<Account>(jsonInput);
+            _accountRepo.AddAccount(newAccount);
+        }
+    }
+}
+
+
+MauiBlazor
+crud to database
+Quick easy simple first
