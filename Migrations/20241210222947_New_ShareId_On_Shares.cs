f82c13dcdd639e1f48a64621108ad5d089cf982b
draven-mcconathy <draveniii@gmail.com>
2024-12-10 17:42:03 -0500

    Added new member to share - ShareId.

---
+++ Migrations/20241210222947_New_ShareId_On_Shares.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCWebBanking.Migrations
+{
+    /// <inheritdoc />
+    public partial class New_ShareId_On_Shares : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Models/Share.cs
+++ Models/Share.cs
@@ -9,6 +9,7 @@ namespace WebBankingApp.Models
     public class Share
     {
         public Int32 Id { get; set; }
+        public Int32 ShareId { get; set; }
         public String Type { get; set; }
         public Decimal MinimumBalance { get; set; }
         public Decimal InterestRate { get; set; }
