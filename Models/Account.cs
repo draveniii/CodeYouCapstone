173745975b64ae1dee60235288aa4dea73d99f1f
draven-mcconathy <draveniii@gmail.com>
2024-12-12 13:10:46 -0500

    Added default and parameterized constructors to Account class

--- Models/Account.cs
+++ Models/Account.cs
@@ -7,5 +7,14 @@ namespace WebBankingApp.Models
         public virtual Int32 Id { get; set; }
         public virtual List<AccountMember> ?Members { get; set; }
         public virtual List<Share> ?Shares { get; } = new List<Share>();
+
+        // Default Constructor
+        public Account() { }
+
+        // Parameterized Constructor
+        public Account (int id)
+        {
+            Id = id;
+        }
     }
 }
