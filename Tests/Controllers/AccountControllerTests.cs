8d1cdc13b1ba54ba439c210f5ee79310823f6b0f
draven-mcconathy <draveniii@gmail.com>
2024-12-12 14:57:01 -0500

    Added AccountControllerTests .cs with tests: LoginGetValid

---
+++ Tests/Controllers/AccountControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Transactions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using MVCWebBanking.Controllers;
+using Xunit;
+using WebBankingApp.Models;
+using WebBankingApp.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+using Microsoft.Data.Sqlite;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using System.Reflection.Metadata;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+
+namespace MVCWebBanking.Tests.Controllers
+{
+    public class AccountControllerTests
+    {
+        [Fact]
+        public async Task LoginGet()
+        {
+            // Arrange
+            AccountsController controller = new AccountsController(null);
+
+            // Act
+            IActionResult result = controller.Login();
+
+            // Assert
+            ViewResult viewResult = Assert.IsType<ViewResult>(result);                             
+        }
+
+        
+    }
+}
